19a1557f65a377ed54f826cb6c95c432eb29b8f2
swiriwon <swiriwon@gmail.com>
2025-04-03 13:10:00 -0700

    Update main.js

--- main.js
+++ main.js
@@ -27,19 +27,21 @@ Actor.main(async () => {
             }
         },
 
-        async preNavigationHooks(crawlingContext, gotoOptions) {
-            const { page } = crawlingContext;
-            await page.setExtraHTTPHeaders({
-                'Accept-Language': 'en-US,en;q=0.9',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                'sec-ch-ua': '"Google Chrome";v="121", " Not;A Brand";v="99"',
-                'sec-ch-ua-mobile': '?0',
-                'sec-ch-ua-platform': '"Windows"',
-                'Upgrade-Insecure-Requests': '1'
-            });
-            gotoOptions.timeout = 90000;
-            gotoOptions.waitUntil = 'networkidle2';
-        },
+            preNavigationHooks: [
+                async (crawlingContext, gotoOptions) => {
+                    const { page } = crawlingContext;
+                    await page.setExtraHTTPHeaders({
+                        'Accept-Language': 'en-US,en;q=0.9',
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                        'sec-ch-ua': '"Google Chrome";v="121", " Not;A Brand";v="99"',
+                        'sec-ch-ua-mobile': '?0',
+                        'sec-ch-ua-platform': '"Windows"',
+                        'Upgrade-Insecure-Requests': '1'
+                    });
+                    gotoOptions.timeout = 90000;
+                    gotoOptions.waitUntil = 'networkidle2';
+                }
+            ],        
 
         async requestHandler({ page, request }) {
             log.info(`Processing: ${request.url}`);
